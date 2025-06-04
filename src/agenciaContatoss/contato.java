7baa6119dac24f030d5026707428a39216b03c64
vitor vicent <vitorvicentee22@gmail.com>
2025-06-04 15:54:18 -0300

    adiciona agenciaContatoss

---
+++ src/agenciaContatoss/contato.java
@@ -0,0 +1,32 @@
+package agenciaContatoss;
+
+public class contato {
+    private String nome;
+    private String telefone;
+
+    public contato(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
